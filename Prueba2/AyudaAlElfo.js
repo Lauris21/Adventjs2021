47711da63240689867bf47d1b7b70f5d6d9fc2bd
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-29 22:22:42 +0100

    feat Prueba2

---
+++ Prueba2/AyudaAlElfo.js
@@ -0,0 +1,3 @@
+// Convertir el texto en un objeto que contenga el nombre del regalo y las veces que aparece.
+// Si tienen un _ al comienzo de la palabra no se deben contar.
+const carta = "bici coche balón _playstation bici coche peluche";
